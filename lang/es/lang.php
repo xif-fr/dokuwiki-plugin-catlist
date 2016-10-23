05edba5925e04585e59d0c25171fe939fd80ee9a
Félix Faisant <xcodexif@xif.fr>
2016-10-23 20:17:57 +0200

    Added Spanish translation

---
+++ lang/es/lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Spanish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Juan Manuel Castillo <juanmanuel.castillo@innn.es>
+ *
+ */
+ 
+$lang['encoding']       = 'utf-8';
+$lang['direction']      = 'ltr';
+$lang['unknownoption']  = 'Opción desconocida %s para &lt;catlist&gt;';
+$lang['outofpages']     = '¡Debes permanecer en el directorio <code>data/pages</code>!';
+$lang['dontexist']      = 'El espacio de nombres <code>%s</code> no existe';
+$lang['addpage']        = 'Añadir página...';
