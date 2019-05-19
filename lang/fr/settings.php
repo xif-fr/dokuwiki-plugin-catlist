5071073e207b20cd90d8c1229edceed895eb19f7
FosseWay <adam@fosseway.dev>
2019-05-19 05:17:11 +0000

    Add French translation of 'showhead' setting description.

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -7,4 +7,5 @@ $lang['nocache'] = "Désactive le cache de la page quand catlist est utilisé.";
 $lang['hide_acl_nsnotr'] = "N'affiche pas les namespaces pour lesquels l'utilisateur n'a pas droit de lecture.";
 $lang['show_acl'] = "Débogage : ignore les ACLs (affiche tout, quelque soit l'utilisateur) et affiche les permissions de l'utilisateur.";
 $lang['useheading'] = "Utilise le premier titre comme nom de page (indépendamment du 'useheading' global)";
+$lang['showhead'] = "Afficher le titre par défaut; remplacement individuel avec -noHead ou -showHead.";
 $lang['show_leading_ns'] = "Affiche le namespace si il existe un élément fils pour lequel l'utilisateur a la permission de lecture.";
