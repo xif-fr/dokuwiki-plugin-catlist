d0bdd4f5964f3b85881035e0bae9478dc9aafaa8
Gerrit Uitslag <klapinklapin@gmail.com>
2023-12-16 20:38:04 +0100

    Replace start config by <start> in fr

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -2,7 +2,7 @@
 
 $lang['default_sort'] = "Ordre de tri par défaut.";
 $lang['hide_index'] = "Ne pas afficher les pages utilisées comme page d'index (page principale d'un namespace)";
-$lang['index_priority'] = "Ordre de priorité des pages utilisées comme page d'index. Exemple pour le namespace foo:bar: start= foo:bar:".$conf['start']." ; outside= foo:bar ; inside= foo:bar:bar";
+$lang['index_priority'] = "Ordre de priorité des pages utilisées comme page d'index. Exemple pour le namespace foo:bar: start= foo:bar:&lt;start&gt; ; outside= foo:bar ; inside= foo:bar:bar";
 $lang['nocache'] = "Désactive le cache de la page quand catlist est utilisé.";
 $lang['hide_acl_nsnotr'] = "N'affiche pas les namespaces pour lesquels l'utilisateur n'a pas droit de lecture.";
 $lang['show_acl'] = "Débogage : ignore les ACLs (affiche tout, quelque soit l'utilisateur) et affiche les permissions de l'utilisateur.";
